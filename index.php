f84e713b7d4f7173963614978328d234c5f0cff6
Diomer Manaois <92076169+DManaois@users.noreply.github.com>
2023-11-18 22:57:59 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -1,16 +1,4 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>User/Admin Selection</title>
-</head>
-<body>
-    <h2>User/Admin Selection</h2>
-    <form method="post" action="index.php">
-        <button type="submit" name="role" value="user">User</button>
-        <button type="submit" name="role" value="admin">Admin</button>
-    </form>
-</body>
-</html>
+
 
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
